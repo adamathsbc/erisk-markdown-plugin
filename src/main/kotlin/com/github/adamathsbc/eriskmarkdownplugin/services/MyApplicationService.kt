12ddee11c781ab906750e1829c6d29c673c6a49f
GitHub Action <action@github.com>
2021-10-12 16:35:03 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/adamathsbc/eriskmarkdownplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.adamathsbc.eriskmarkdownplugin.services
+
+import com.github.adamathsbc.eriskmarkdownplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
